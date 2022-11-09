753b40b8fb7fe81997ca4bab77f2891b1b24ceb7
dginix <zero>
2022-11-09 09:26:21 +0300

    feat: Добавлена модель для User

---
+++ src/main/java/ru/yandex/praktikum/catsgram/model/User.java
@@ -0,0 +1,10 @@
+package ru.yandex.praktikum.catsgram.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String id;
+    private String username;
+    private String nickname;
+}
